ece55e1b6c4b0a8b0591ab2139c1b955c76f1a50
Alexander Santos <alexsantosantana@live.com>
2019-07-23 20:11:58 -0400

    Initial setup of my restful API

--- server.js
+++ server.js
@@ -37,12 +37,49 @@ var owners = [
 
 
 // GET /api/owners
+app.get('/api/owners', (res, req, next) => {
+    res.json(owners)
+})
 
 // GET /api/owners/:id
+app.get('/api/owners/:id', (res, req, next) => {
+    let owner = owners.find(owner => owner.id === parseInt(req.params.id))
+    if (owner) {
+        res.json(owner)
+    } else {
+        res.status(404).json({ error: "Owner not found" })
+    }
+})
 
 // POST /api/owners
+app.post('/api/owners', (res, req, next) => {
+    let nextId = Math.max(owners.map(owner => owner.id)) + 1
+    if (req.body.name) {
+        let owner = {
+            id: nextId,
+            name: req.body.name,
+            pets: req.body.pets == nul ? "None" : req.body.pets
+        }
+        owners.push(owner)
+        res.json(owner)
+    } else {
+        res.statusCode(400).json({ error: 'Sorry you need to add a name &' })
+    }
+})
 
 // PUT /api/owners/:id
+app.put('/api/owners/:id', (res, req, next){
+    let owner = owners.find(owner => owner.id === parseInt(req.params.id))
+    if (owner) {
+        owner = req.body.params;
+        res.json({
+            owner: owner,
+            status: 'Updated'
+        })
+    } else {
+        res.statusCode(400).json({ error: "Owner not found" })
+    }
+})
 
 // DELETE /api/owners/:id
 
@@ -57,6 +94,6 @@ var owners = [
 // DELETE /api/owners/:id/pets/:petId
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Pets API is now listening on port 3000...');
 })
